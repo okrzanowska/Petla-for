0fcfebc6aff987d460ae3ced207601319f8ad8e8
oliwi <oliwi@OLIWIA>
2023-12-09 18:33:50 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PetlaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x=0; x<=10; x++){
+                double y = Math.Sqrt(Math.Pow(3, 2)) * x;
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
